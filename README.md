f1b08da682b0d148dabdc4123e6f5866c73d1bd8
Sathyanarayanan-ops <57038667+Sathyanarayanan-ops@users.noreply.github.com>
2025-03-19 19:18:35 -0400

    Websearch functionality added with tavily

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # EmailAgent
+
+
+First set up lang graph , 
+
+Learned concepts - State 
+
+Next is Tavily search engine to give llm the ability to handle websearch 
+
--- chatbot.py
+++ chatbot.py
@@ -39,6 +39,39 @@ print(f"✅ GROQ_API_KEY is loaded: {bool(GROQ_API_KEY)}")
 llm = ChatGroq(model="llama3-8b-8192")
 
 
+# Integrating Tavily search
+
+from tavily import TavilyClient
+
+
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+# tavily_client = TavilyClient(api_key=TAVILY_API_KEY)
+
+# response = tavily_client.search("Who is Leo Messi?")
+
+# print(response)
+
+
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_community.utilities.tavily_search import TavilySearchAPIWrapper
+
+
+
+tavily_api_wrapper = TavilySearchAPIWrapper(tavily_api_key=TAVILY_API_KEY)
+
+tool = TavilySearchResults(max_results=2, api_wrapper = tavily_api_wrapper)
+tools = [tool]
+tool.invoke("What's a 'node' in LangGraph?")
+
+
+
+
+
+llm_with_tools = llm.bind_tools(tools)
+
+
+
+
 
 
 class State(TypedDict):
@@ -50,12 +83,18 @@ class State(TypedDict):
 
 graph_builder = StateGraph(State)
 
+def chatbot(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
 
-
-def chatbot(state : State):
-    return {"messages": [llm.invoke(state["messages"])]}
 
 graph_builder.add_node("chatbot", chatbot)
+
+
+
+# def chatbot(state : State):
+#     return {"messages": [llm.invoke(state["messages"])]}
+
+# graph_builder.add_node("chatbot", chatbot)
 
 
 
@@ -71,6 +110,119 @@ def stream_graph_updates(user_input: str):
     for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
         for value in event.values():
             print("Assistant:", value["messages"][-1].content)
+
+
+# while True:
+#     try:
+#         user_input = input("User: ")
+#         if user_input.lower() in ["quit", "exit", "q"]:
+#             print("Goodbye!")
+#             break
+
+#         stream_graph_updates(user_input)
+#     except:
+#         # fallback if input() is not available
+#         user_input = "What do you know about LangGraph?"
+#         print("User: " + user_input)
+#         stream_graph_updates(user_input)
+#         break
+    
+    
+    
+    
+
+# Tavily api
+
+
+
+# Below, we implement a BasicToolNode that checks the most recent message
+# in the state and calls tools if the message contains tool_calls. 
+
+# We will later replace this with LangGraph's prebuilt ToolNode to speed things up,
+# but building it ourselves first is instructive.
+
+import json
+
+from langchain_core.messages import ToolMessage
+
+
+class BasicToolNode:
+    """A node that runs the tools requested in the last AIMessage."""
+
+    def __init__(self, tools: list) -> None:
+        self.tools_by_name = {tool.name: tool for tool in tools}
+
+    def __call__(self, inputs: dict):
+        if messages := inputs.get("messages", []):
+            message = messages[-1]
+        else:
+            raise ValueError("No message found in input")
+        outputs = []
+        for tool_call in message.tool_calls:
+            tool_result = self.tools_by_name[tool_call["name"]].invoke(
+                tool_call["args"]
+            )
+            outputs.append(
+                ToolMessage(
+                    content=json.dumps(tool_result),
+                    name=tool_call["name"],
+                    tool_call_id=tool_call["id"],
+                )
+            )
+        return {"messages": outputs}
+
+
+tool_node = BasicToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
+
+'''
+
+With the tool node added, we can define the conditional_edges.
+
+Recall that edges route the control flow from one node to the next.
+Conditional edges usually contain "if" statements to route to different nodes depending
+on the current graph state.
+These functions receive the current graph state and return a string or list
+of strings indicating which node(s) to call next.
+
+'''
+
+
+
+def route_tools(
+    state: State,
+):
+    """
+    Use in the conditional_edge to route to the ToolNode if the last message
+    has tool calls. Otherwise, route to the end.
+    """
+    if isinstance(state, list):
+        ai_message = state[-1]
+    elif messages := state.get("messages", []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError(f"No messages found in input state to tool_edge: {state}")
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        return "tools"
+    return END
+
+
+# The `tools_condition` function returns "tools" if the chatbot asks to use a tool, and "END" if
+# it is fine directly responding. This conditional routing defines the main agent loop.
+graph_builder.add_conditional_edges(
+    "chatbot",
+    route_tools,
+    # The following dictionary lets you tell the graph to interpret the condition's outputs as a specific node
+    # It defaults to the identity function, but if you
+    # want to use a node named something else apart from "tools",
+    # You can update the value of the dictionary to something else
+    # e.g., "tools": "my_tools"
+    {"tools": "tools", END: END},
+)
+# Any time a tool is called, we return to the chatbot to decide the next step
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge(START, "chatbot")
+graph = graph_builder.compile()
 
 
 while True:
